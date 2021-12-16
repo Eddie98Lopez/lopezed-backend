a0dd70a48a06304bd3627bd71c773111964b6f8f
Eduardo Lopez <ed98lopez@gmail.com>
2021-12-15 21:06:10 -0800

    added messages and categories table schemas to migration

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -6,8 +6,24 @@ exports.up = async (knex) => {
       users.string('password', 200).notNullable()
       users.timestamps(false, true)
     })
+    await knex.schema
+      .createTable('messages',(messages)=>{
+        messages.increments('message_id')
+        messages.string('first').notNullable()
+        messages.string('last').notNullable()
+        messages.integer('phone')
+        messages.string('email').notNullable()
+        messages.string('message').notNullable()
+      })
+    await knex.schema
+      .createTable('categories', (categories)=>{
+        categories.increments('category_id')
+        categories.string('category').notNullable()
+      })
 }
 
 exports.down = async (knex) => {
+  await knex.schema.dropTableIfExists('categories')
+  await knex.schema.dropTableIfExists('messages')
   await knex.schema.dropTableIfExists('users')
 }
